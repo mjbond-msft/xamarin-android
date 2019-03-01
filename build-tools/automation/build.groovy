bd8346f0265ce9eb13f694e37804e71f74e58b7f
Mike Bond <mjbond-msft@outlook.com>
2019-03-01 10:19:16 -0800

    Another bogus comment

--- build-tools/automation/build.groovy
+++ build-tools/automation/build.groovy
@@ -1,4 +1,5 @@
 // UNDONE: Bogus comment as change to pr-eval branch
+// UNDONE: Another bogus comment as change to pr-eval branch
 // This file is based on the Jenkins scripted pipeline (as opposed to the declarative pipeline) syntax
 // https://jenkins.io/doc/book/pipeline/syntax/#scripted-pipeline
 import groovy.json.JsonSlurper
